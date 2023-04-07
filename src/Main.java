bc6c6311f4fc2062999e96ae5003d232c8a9dcea
mahfooz alam <mahfoozalam1070@gmail.com>
2023-04-07 21:41:41 +0530

    commit

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,29 @@
-import java.util.Scanner;
 public class Main {
-    public static void main(String[] args) {
-        Scanner s = new Scanner(System.in);
-        System.out.println("Enter the radius:");
-        double r = s.nextDouble();
-        double area = (22 * r * r) / 7;
-        System.out.println("Area of Circle is:" + area);
+
+public static void main(String[] args) {
+    //WAP to display The list even number
+    //Wap to print factorial number
+    int num1 = 324;
+    int num2=  234;
+    if (num1 > num2){
+        System.out.println(num1 +" is greater than" + num2);
+
     }
+    else  if (num1 < num2){
+        System.out.println(num1 + " is less than" + num2);
+    }
+    else {
+        System.out.println(num1 + " is equal to" + num2);
+    }
+
+
+
+
+
+
+
+
+}
 }
 
 
