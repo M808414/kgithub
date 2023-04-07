9741732a8ed295e0d5ff6ab08c7ab4ea146a2151
mahfooz alam <mahfoozalam1070@gmail.com>
2023-04-07 22:04:02 +0530

    commit

---
+++ DetermineLeapYear.java
@@ -0,0 +1,9 @@
+package PACKAGE_NAME;public class DetermineLeapYear {
+    public static void main(String[] args){
+        int year = 2004;
+        if ((year % 400 = 0) || (year % 4 == 0) && (year % 100  != 0))
+            System.out.println("year " + year +" is a leap year");
+        else
+            System.out.println("Year" + year + " is not a leap year");
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -3,18 +3,6 @@ public class Main {
 public static void main(String[] args) {
     //WAP to display The list even number
     //Wap to print factorial number
-    int num1 = 324;
-    int num2=  234;
-    if (num1 > num2){
-        System.out.println(num1 +" is greater than" + num2);
-
-    }
-    else  if (num1 < num2){
-        System.out.println(num1 + " is less than" + num2);
-    }
-    else {
-        System.out.println(num1 + " is equal to" + num2);
-    }
 
 
 
